2d8a0c0095ea85915862de11f601a77f36251066
Dexda27 <dekda2003@gmail.com>
2023-06-12 09:40:10 +0800

    gambar bg

---
+++ formsewa.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+  <title>Form Sewa</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+  <title>Hello, world!</title>
+</head>
+
+
+
+<body background="Avansa.png">
+
+  <h4 align="center">FORM SEWA</h4>
+  <hr />
+
+  <table border="3" align="center">
+
+    <tr>
+      <td>Masukkan Nama</td>
+    </tr>
+    <tr>
+      <td><input type="text" placeholder="" name="nama" size="40"></td>
+    </tr>
+
+    <tr>
+      <td>Masukkan No Handphone</td>
+    </tr>
+    <tr>
+      <td><input type="text" placeholder="" name="NoHp" size="40"></td>
+    </tr>
+
+    <tr>
+      <td>Masukkan No KTP/NIK </td>
+    </tr>
+    <tr>
+      <td><input type="text" placeholder="" name="No Ktp" size="40"></td>
+    </tr>
+
+    <tr>
+      <td>Tanggal Peminjaman</td>
+    </tr>
+    <tr>
+      <td><input type="date" placeholder="" name="tglpinjam"></td>
+    </tr>
+
+    <tr>
+      <td>Tanggal Pengembalian</td>
+    </tr>
+    <tr>
+      <td><input type="date" placeholder="" name="tglkembali"></td>
+    </tr>
+
+    <tr>
+      <td>Lama Peminjaman(Hari)</td>
+    </tr>
+
+    <tr>
+      <td><input type="number" placeholder="" name="lamapinjam" size="40"></td>
+    </tr>
+
+    <tr>
+      <td>
+        <input type="submit" value="Kirim" />
+        <input type="reset" value="Batal" />
+      </td>
+    </tr>
+
+  </table>
+
+  <!-- Optional JavaScript; choose one of the two! -->
+
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
