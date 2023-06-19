b0541c50d8aa0b5dd9426be619b80d5033c99c8b
IMadeAgus <agsmade@gmail.com>
2023-06-19 10:03:49 +0800

    final Web User

---
+++ final.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Sewa Mobil</title>
+    <title>Project 1</title>
+    <!-- Bootsrap -->
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="fontawesome/css/all.css" />
+    <!-- CSS Custom -->
+    <link rel="stylesheet" href="css/style.css" />
+    <!-- Aos -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- link swetalert2 -->
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top bg-navbar navbar-custom">
+        <div class="container  py-2">
+            <a class="navbar-brand text-white" href="#">Bravo Rent Cars</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html#skills">Sewa Mobil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portofolio">Retur Mobil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php" onclick="return confirm('Yakin mau Log Out?')">Log Out</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- //End Navbar Session -->
+
+    <!-- Banner -->
+
+    <div class="hero-section">
+        <div class="hero-section-text">
+            <h1>Bravo Rent Car</h1>
+            <h5>Drive in Paradise, Feel the Bravo Vibes!</h5>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-8 text-black content-">
+                <h4 class="fw-bold">Welcome To Bravo Rent Car</h4>
+                <br>
+                <p>
+                    Travel around Bali in a new car with your personal, English’s speaking driver.
+                </p>
+                <br>
+                <p>
+                    Get 10 hours daily service, fuel and unlimited mileage to go just anywhere you want!
+                    Plan any tour, get a ride to beaches, shops and restaurants.
+                </p>
+                <br>
+                <p>
+                    Book without credit card, change or cancel reservation any time, free of charge.
+                </p>
+                <br>
+                <div class="my-2">
+                    <a href="propage.html" class="btn tombol"> Book a Car Now</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!--//End Banner session  -->
+
+    <!-- Promo -->
+    <div class="promo-section row mt-5">
+        <div class="promo-section-text col-8">
+            <h1>Bravo Rent Car Best Offers</h1>
+            <br>
+            <h4>
+                * Start from USD 26 / hours (Half Day Tours)
+
+
+            </h4>
+            <br>
+            <h4>
+                * Get free one cup of Luwak coffee in coffee plantation
+            </h4>
+            <br>
+            <h4>
+                * Get free banana boat for one person (If book more then two days).
+            </h4>
+            <br>
+            <div class="my-2">
+                <a href="propage.html" class="btn tombol"> Book a Car Now</a>
+            </div>
+        </div>
+    </div>
+    <!--//End Promo session -->
+
+    <!-- Service -->
+
+
+    <!--//End Service session  -->
+</body>
+
+</html>
